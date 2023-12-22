ec3d4670c4dd1de37e4dcffbd7c14eea4695fb26
MohaSetif <BouroubaMohamed004@gmail.com>
2023-12-22 10:01:40 +0100

    adding css

--- src/App.java
+++ src/App.java
@@ -1,24 +1,24 @@
-import javafx.application.Application;  
+import java.io.IOException;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;  
 public class App extends Application{  
   
-    @Override  
-    public void start(Stage primaryStage) throws Exception {  
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         try {
-            Parent root= FXMLLoader.load(getClass().getResource("EntryScene.fxml"));  
-            Scene scene=new Scene(root);
-            //String css = this.getClass().getResource("style.css").toExternalForm();
-            //scene.getStylesheets().add(css);
-            primaryStage.setTitle("Encryptify");  
-            primaryStage.setScene(scene);  
-            primaryStage.show(); 
-        } catch (Exception e) {
+            Parent root = FXMLLoader.load(getClass().getResource("EntryScene.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Encryptify");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
             e.printStackTrace();
-        } 
-    }  
+        }
+    }
     public static void main (String[] args)  
     {  
         launch(args);  
